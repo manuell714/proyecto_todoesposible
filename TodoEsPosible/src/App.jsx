26fcf368dce145c5e91336c70cecc0ec3dce6fc4
elizabethgutierrez27 <146129308+elizabethgutierrez27@users.noreply.github.com>
2025-06-05 19:52:50 -0600

    Creación del feature/content

--- TodoEsPosible/src/App.jsx
+++ TodoEsPosible/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/footer'
+import Content from './components/content'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Content/>
       <Footer/>
     </>
   )
